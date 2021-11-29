4a8ce34bc59a64b605ca6593265de9502622121c
mivvlk <mivvlk@yandex.ru>
2021-11-29 12:03:49 +0300

    Добавьте файлы проекта.

---
+++ 1 - inJSON/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.IO;
+using System.Text.Json.Serialization;
+
+namespace _1_inJSON
+{
+    /*
+     * Необходимо разработать программу для записи информации о товаре в текстовый файл в формате json. 
+     * 
+     * Разработать класс для моделирования объекта «Товар». 
+     * Предусмотреть члены класса «Код товара» (целое число), «Название товара» (строка), «Цена товара» (вещественное число). 
+     * Создать массив из 5-ти товаров, значения должны вводиться пользователем с клавиатуры. 
+     * Сериализовать массив в json-строку, сохранить ее программно в файл «Products.json».
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filesName = "Products.json";
+            int n = 5;
+            Console.WriteLine("Введите {0} товаров:", n);
+            Product[] productMas = new Product[n];
+            for (int i = 0; i < n; i++)
+            {
+                Product product = new Product();
+                Console.Write("Название {0} товара: ", i + 1);
+                product.Name = Console.ReadLine();
+                Console.Write("Код {0} товара: ", i + 1);
+                try
+                {
+                    product.Code = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Ошибка! {0}", ex.Message);
+                }
+                Console.Write("Цену {0} товара: ", i + 1);
+                try
+                {
+                    product.Price = Convert.ToDouble(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Ошибка! {0}", ex.Message);
+                }
+                productMas[i] = product;
+            }
+
+            JsonSerializerOptions option = new JsonSerializerOptions
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true
+            };
+            string strJSON = "";
+            for (int i = 0; i < n; i++)
+            {
+                strJSON = strJSON + JsonSerializer.Serialize(productMas[i], option);
+                strJSON = strJSON + "\n";
+            }
+
+            WorkDirFiles.WriteTxtFiles(filesName, strJSON);
+            Console.WriteLine(strJSON);
+            Console.ReadKey();
+        }
+
+    }
+    public class Product
+    {
+        
+        int code;        
+        double price;
+        [JsonPropertyName("Название товара")]
+        public string Name { get; set; }
+        [JsonPropertyName("Код товара")]
+        public int Code
+        {
+            get
+            {
+                return code;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    code = value;
+                }
+            }
+        }
+        [JsonPropertyName("Цена товара")]
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    price = value;
+                }
+            }
+        }
+    }
+    public static class WorkDirFiles
+    {
+        public static void CreateDir(string path)
+        {
+            if ((!Directory.Exists(path)) && (path.Length > 0))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+        public static void WriteTxtFiles(string filePath, params string[] data)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(filePath))
+                {
+                    foreach (var str in data)
+                    {
+                        sw.WriteLine(str);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка! {0}", ex.Message);
+            }
+        }
+        public static void ReadTxtFiles(string filePath, ref string[] data)
+        {
+            Array.Clear(data, 0, data.Length);
+            try
+            {
+
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    data = sr.ReadToEnd().Split('\n');
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка! {0}", ex.Message);
+            }
+        }
+        public static void ReadTxtFiles(string filePath, ref string data)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    data = sr.ReadToEnd();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка! {0}", ex.Message);
+            }
+        }
+    }
+}
---
+++ 2 - outJSON/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using _1_inJSON;
+
+namespace _2_outJSON
+{
+    /*
+     * Необходимо разработать программу для получения информации о товаре из json-файла. 
+     * Десериализовать файл «Products.json» из задачи 1. 
+     * Определить название самого дорогого товара.
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"..\..\..\1 - inJSON\bin\Debug\Products.json";
+            string strJSON = "";
+            WorkDirFiles.ReadTxtFiles(fileName, ref strJSON);
+
+            /* В случае оформления файла*.json с пробелами (красивое оформление для печати) и без этого оформления, 
+             * переводим в формат построчного исполнения, т.е. строка заключенная в {} на новой строке 
+             */
+
+            string[] strJSONMas= { };
+            StringJSONMas(strJSON, ref strJSONMas);
+            
+            //Определяем кол-во продуктов и оформляем их в массив объектов
+            int
+                nMas = strJSONMas.Length,
+                index = 0;
+
+            Product[] productMas = new Product[nMas];
+            foreach (var str in strJSONMas)
+            {
+                if (str.Length > 0)
+                {
+                    try
+                    {
+                        productMas[index] = JsonSerializer.Deserialize<Product>(str);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Ошибка! {0}", ex.Message);
+                    }
+                    index++;
+                }
+            }
+
+            //Определяем результат
+            double priceProduct = 0;
+            string nameProduct = "";
+            for (int i = 0; i < nMas; i++)
+            {
+                if(priceProduct< productMas[i].Price)
+                {
+                    priceProduct = productMas[i].Price;
+                    nameProduct = productMas[i].Name;
+                }
+            }  
+
+            Console.WriteLine("Самый дорогой товар: {0} его цена {1} руб.",nameProduct, priceProduct);
+            Console.ReadKey();
+        }
+        static void StringJSONMas(string JSON,ref string[] strMas)
+        {
+            string[] strJSONMas = JSON.Split('\r');
+            string strJSON = "";
+            foreach (var str in strJSONMas)
+            {
+                str.Trim();
+                strJSON += str.Trim();
+            }
+            strMas = strJSON.Split('\n');
+        }
+    }
+}
